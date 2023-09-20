5e33f50f4918ff2efc4dc607363bc2627c0fb247
JohanAcosta19 <98934936+JohanAcosta19@users.noreply.github.com>
2023-09-19 22:30:37 -0600

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,7 +1,7 @@
 var fibonacci = [];
 fibonacci[0] = 0;
 fibonacci[1] = 1;
-for (var i = 2; i < 100; i++)
+for (var i = 2; i < 100; i++) //"100" es el número de veces que hace la operacion pero los primeros 2 resultados son irrelevantes, no supe como solucionarlo
 {
   fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 }
